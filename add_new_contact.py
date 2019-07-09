7fe1d576a645272656b692e43198cefe7a04fe48
Elena Pavlova <e.pavlova@bi.zone>
2019-07-09 14:16:44 +0300

    Создан вспомогательный класс Contact для хранения свойств контакта

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import unittest
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from contact import Contact
 
 class AddNew(unittest.TestCase):
     def setUp(self):
@@ -11,44 +12,40 @@ class AddNew(unittest.TestCase):
 
     def test_add_new(self):
         wd = self.wd
-        self.create_new_contact(wd)
-
-    def create_new_contact(self, wd):
-        self.open_home_page(wd)
+        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd)
+        self.add_new_contact(wd, Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog", company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru"))
+        self.logout(wd)
 
-    def add_new_contact(self, wd):
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+        wd.implicitly_wait(30)
+
+    def add_new_contact(self, wd, contact):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Ivan")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Ivanovich")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Ivanov")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Rog")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("OOO")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Moscow")
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Address:'])[1]/following::label[1]").click()
-        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+79260001111")
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("123@mail.ru")
+        wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
         wd.find_element_by_name("bday").click()
@@ -61,7 +58,7 @@ class AddNew(unittest.TestCase):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
 
-    def open_home_page(self, wd):
+    def open_homepage(self, wd):
         wd.get("http://localhost:8080/addressbook/")
 
     def login(self, wd, username, password):

ce0efed6cc05635aaa960580282e6fd23c703dd7
Elena Pavlova <e.pavlova@bi.zone>
2019-07-14 16:53:08 +0300

    внесены изменения в тест, добавлено обращение через session

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 
 def test_add_new(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.add_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru",
                                          bday="1", bmonth="January", byear="1990"))
-    app.logout()
+    app.session.logout()
 
 

92d82568f7cae2508a2764b5fd50d950141df243
Elena Pavlova <e.pavlova@bi.zone>
2019-07-15 16:34:54 +0300

    фикстура conftest сделана общей для всех тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-
-import pytest
 from model.group import Group
-from fixture.application import Application
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="group_name", header="header", footer="footer"))
     app.session.logout()
-
 
 def test_add_emty_group(app):
     app.session.login(username="admin", password="secret")

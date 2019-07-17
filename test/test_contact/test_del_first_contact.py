7b865150bcbe491141ef7bb301ff4d399765cf5b
Elena Pavlova <e.pavlova@bi.zone>
2019-07-17 14:41:53 +0300

    Переименован сценарий удаления

--- test/test_contact/test_del_first_contact.py
+++ test/test_contact/test_del_first_contact.py
@@ -1,7 +1,7 @@
 
 from model.contact import Contact
 
-def test_del_contact(app):
+def test_del_first_contact(app):
     app.session.login( username="admin", password="secret")
     app.contact.add_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru",

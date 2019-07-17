95a4048830ef027708fcd4aa6a99758dbfc1aed4
Elena Pavlova <e.pavlova@bi.zone>
2019-07-17 13:33:29 +0300

    Изменена структура Проекта: тесты разбиты на 2 папки: - test_group - test_contact

---
+++ test/test_contact/test_del_contact.py
@@ -0,0 +1,14 @@
+
+from model.contact import Contact
+
+def test_add_new(app):
+    app.session.login( username="admin", password="secret")
+    app.contact.add_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
+                                         company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru",
+                                         bday="1", bmonth="January", byear="1990"))
+    app.session.logout()
+
+def test_del_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.del_first_contact()
+    app.session.logout()

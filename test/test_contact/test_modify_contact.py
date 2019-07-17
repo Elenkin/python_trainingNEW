cdb95f9cf6ecbac8a49e3bbf1e4cebf78605779d
Elena Pavlova <e.pavlova@bi.zone>
2019-07-17 13:57:03 +0300

    Переименован сценарий

--- test/test_contact/test_modify_contact.py
+++ test/test_contact/test_modify_contact.py
@@ -1,8 +1,8 @@
 from model.contact import Contact
 
-def test_contact_details(app):
+def test_modify_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.details_modify(Contact(firstname="NEW", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
+    app.contact.details_modify(Contact(firstname="", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru",
                                          bday="1", bmonth="January", byear="1990"))
     app.session.logout()

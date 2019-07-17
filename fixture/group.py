f5b2e65e13e6f8e51317141db89e1c38ac89ffb9
Elena Pavlova <e.pavlova@bi.zone>
2019-07-17 15:15:45 +0300

    Метод создания группы разделен на 2: - create - fill_group_form

--- fixture/group.py
+++ fixture/group.py
@@ -11,7 +11,12 @@ class groupHelper:
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
-        # fill group form
+        self.fill_group_form(group)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -21,9 +26,6 @@ class groupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
 
     def open_group_page(self):
         wd = self.app.wd
@@ -37,6 +39,12 @@ class groupHelper:
         wd.find_element_by_name("selected[]").click()
         #submit deletion
         wd.find_element_by_name("delete").click()
+
+    def edit(self):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click()
+
 
 
 

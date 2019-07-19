bbfd3702e59715707c47f36f0f929b190cc63495
Elena Pavlova <e.pavlova@bi.zone>
2019-07-19 17:41:14 +0300

    Изменен метод modify group, в редактировании можно передавать только изменяемый параметр

--- test/test_group/test_edit_group.py
+++ test/test_group/test_edit_group.py
@@ -4,3 +4,8 @@ def test_edit_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit(Group(name="NEW_group", header="header", footer="footer"))
     app.session.logout()
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="NEW_group"))
+    app.session.logout()

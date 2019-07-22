9e1625091a6615c43bff469d7fc4abd0148d7057
Elena Pavlova <e.pavlova@bi.zone>
2019-07-22 13:54:56 +0300

    В фикстуру conftest перенесены методы login/logout, для авторизации 1 раз за время прохождения тестов

--- test/test_group/test_edit_group.py
+++ test/test_group/test_edit_group.py
@@ -1,11 +1,7 @@
 from model.group import Group
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit(Group(name="NEW_group", header="header", footer="footer"))
-    app.session.logout()
 
 def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="NEW_group"))
-    app.session.logout()

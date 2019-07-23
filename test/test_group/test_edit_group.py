370d13b27074b84d51e8d105d77a98dba218fb94
Elena Pavlova <e.pavlova@bi.zone>
2019-07-23 10:54:11 +0300

    Добавлена проверка наличия хотя бы одного элемента, который можно модифицировать, и если такого элемента нет -- предварительно создаем его

--- test/test_group/test_edit_group.py
+++ test/test_group/test_edit_group.py
@@ -1,7 +1,11 @@
 from model.group import Group
 
 def test_edit_group(app):
+    if app.group.count_group() == 0:
+        app.group.create(Group(name="test", header="header", footer="footer"))
     app.group.edit(Group(name="NEW_group", header="header", footer="footer"))
 
 def test_edit_first_group(app):
-    app.group.modify_first_group(Group(name="NEW_group"))
+    if app.group.count_group() == 0:
+        app.group.create(Group(name="test", header="header", footer="footer"))
+    app.group.modify_first_group(Group(name="Modify_name"))

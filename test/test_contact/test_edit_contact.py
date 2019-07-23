b9335ed23b9dfac28f3a67ef84b3215c1f90bcaf
Elena Pavlova <e.pavlova@bi.zone>
2019-07-23 12:38:12 +0300

    Добавлена проверка наличия хотя бы одного контакта, который можно Edit/Modify, и если такого элемента нет -- предварительно создаем его

--- test/test_contact/test_edit_contact.py
+++ test/test_contact/test_edit_contact.py
@@ -1,6 +1,10 @@
 from model.contact import Contact
 
 def test_edit_contact(app):
+    if app.contact.count_contact() == 0:
+        app.contact.add_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
+                                         company="OOO", address="Moscow", mobile="+79260003333", email="123@mail.ru",
+                                         bday="1", bmonth="January", byear="1990"))
     app.contact.edit(Contact(firstname="Edit", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260003333", email="123@mail.ru",
                                          bday="1", bmonth="January", byear="1990"))
--- test/test_contact/test_modify_contact.py
+++ test/test_contact/test_modify_contact.py
@@ -1,6 +1,10 @@
 from model.contact import Contact
 
 def test_modify_contact(app):
+    if app.contact.count_contact() == 0:
+        app.contact.add_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
+                                         company="OOO", address="Moscow", mobile="+79260003333", email="123@mail.ru",
+                                         bday="1", bmonth="January", byear="1990"))
     app.contact.details_modify(Contact(firstname="Modify", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260002222", email="123@mail.ru",
                                          bday="1", bmonth="January", byear="1990"))

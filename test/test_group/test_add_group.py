e59d1f6d8eee69fccbea75007c6177dc16f870b9
Elena Pavlova <e.pavlova@bi.zone>
2019-07-31 17:36:11 +0300

    Добавлена дополнительная проверка: Проверяем Старые СПИСКИ групп с Новым СПИСКОМ групп

--- test/test_group/test_add_group.py
+++ test/test_group/test_add_group.py
@@ -2,9 +2,15 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group_name", header="header", footer="footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_emty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
--- test/test_group/test_edit_group.py
+++ test/test_group/test_edit_group.py
@@ -3,9 +3,15 @@ from model.group import Group
 def test_edit_group(app):
     if app.group.count_group() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="NEW_group", header="header", footer="footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_first_group(app):
     if app.group.count_group() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="Modify_name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

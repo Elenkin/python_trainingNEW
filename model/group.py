5bb3485325fb313d4c3600d0b3588a54641638dd
Elena Pavlova <e.pavlova@bi.zone>
2019-08-01 15:13:42 +0300

    Добавлена дополнительная проверка: Сравнение списка Групп, добавлен метод проверки ID у группы

--- model/group.py
+++ model/group.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 
 class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -10,6 +11,12 @@ class Group:
     def __repr__(self):
         return "%s:%s" % (self.id, self.name)
 
-    #сравнение объектов по смыслу
+    #сравнение объектов по смыслу (поэлементное сравнение двух списков)
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(gr):
+        if gr.id:
+            return int(gr.id)
+        else:
+            return maxsize

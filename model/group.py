9aecebc43b6d4e67d22ef629bbf9eb978cffe176
Elena Pavlova <e.pavlova@bi.zone>
2019-08-01 14:36:36 +0300

    Добавлена дополнительная проверка: Удалили элемент списка и сравнила списки. Также добавлена фанкция сравнения Объектов Списка

--- model/group.py
+++ model/group.py
@@ -5,3 +5,11 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    #как выглядит строковое представление Объекта выведенного в консоли
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    #сравнение объектов по смыслу
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name

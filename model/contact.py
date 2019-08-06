fd4b327886a453988ab120eaf0dec88bed4a82c2
Elena Pavlova <e.pavlova@bi.zone>
2019-08-06 14:37:27 +0300

    Добавлена проверка сравнения списков в test_add_new_contact

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 
 class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, company=None, address=None, mobile=None, email=None, bday=None, bmonth=None, byear=None, id=None):
@@ -18,6 +19,14 @@ class Contact:
         return "%s: %s %s" % (self.id, self.lastname, self.firstname)
 
     def __eq__(self, other):
-        return self.id == other.id and self.lastname == other.lastname and self.firstname == other.firstname
+        return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname and self.firstname == other.firstname
+
+    def id_or_max(gr):
+        if gr.id:
+            return int(gr.id)
+        else:
+            return maxsize
+
+
 
 
--- test/test_contact/test_add_new_contact.py
+++ test/test_contact/test_add_new_contact.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
+
 def test_add_new(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.add_new_contact(Contact(firstname="name", middlename="Ivanovich", lastname="Petrov", nickname="Rog",
+    #локальная переменная которая создает Контакт
+    contact = Contact(firstname="name", middlename="Ivanovich", lastname="Petrov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260001111", email="123@mail.ru",
-                                         bday="1", bmonth="January", byear="1990"))
+                                         bday="1", bmonth="January", byear="1990")
+    app.contact.add_new_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 

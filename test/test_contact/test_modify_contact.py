f8c751090740452ed334a61bb18f17600db4d541
Elena Pavlova <e.pavlova@bi.zone>
2019-08-06 14:46:56 +0300

    Добавлена проверка сравнения списков в test_modify_contact

--- test/test_contact/test_modify_contact.py
+++ test/test_contact/test_modify_contact.py
@@ -6,8 +6,13 @@ def test_modify_contact(app):
                                          company="OOO", address="Moscow", mobile="+79260003333", email="123@mail.ru",
                                          bday="1", bmonth="January", byear="1990"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.details_modify(Contact(firstname="Modify", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
+    contact = Contact(firstname="Modify", middlename="Ivanovich", lastname="Ivanov", nickname="Rog",
                                          company="OOO", address="Moscow", mobile="+79260002222", email="123@mail.ru",
-                                         bday="1", bmonth="January", byear="1990"))
+                                         bday="1", bmonth="January", byear="1990")
+    #запомнили id
+    contact.id = old_contacts[0].id
+    app.contact.details_modify(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

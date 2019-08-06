1f012cf8253ef257145885b86a3af0f6b720b208
Elena Pavlova <e.pavlova@bi.zone>
2019-08-06 16:37:06 +0300

    Хеширование: добавили метод group.count_group() который ускоряет операции

--- test/test_group/test_add_group.py
+++ test/test_group/test_add_group.py
@@ -6,18 +6,19 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="group_name", header="header", footer="footer")
     app.group.create(group)
+    #сравнимаем не со значением длины нового списка а со значениеv rjnjhsq djpdhfoftn метод count
+    assert len(old_groups) + 1 == app.group.count_group()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert  sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_add_emty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_emty_group(app):
+    #old_groups = app.group.get_group_list()
+   #group = Group(name="", header="", footer="")
+    #app.group.create(group)
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) + 1 == len(new_groups)
+    #old_groups.append(group)
+    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 

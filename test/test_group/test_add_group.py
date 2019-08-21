ffa29d1f79a3bafe0d4be9371cf9b3f25e5b6396
Elena Pavlova <e.pavlova@bi.zone>
2019-08-21 20:30:23 +0300

    Добавлена загрузка тестовых данных из файла json для теста создание Группы

--- test/test_group/test_add_group.py
+++ test/test_group/test_add_group.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-from data.add_group import testdata
+from data.groups import testdata
 
 
 #добавили передачу тестовых данных в качестве параметра, где group- параметр, testdata - источник, ids - параметр с текстовым представлением
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+#@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     #сравнимаем не со значением длины нового списка а со значениеv rjnjhsq djpdhfoftn метод count
